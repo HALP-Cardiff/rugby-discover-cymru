16458d2e202e44cd6f8bc85148303f568b938e19
Prem Jha <premjha3010@gmail.com>
2026-02-07 14:45:19 +0000

    Adding photos to the organisation

--- rugby_discover_cymru/next.config.ts
+++ rugby_discover_cymru/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "public.wru.wales",
+        pathname: "/organisation/logos/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
