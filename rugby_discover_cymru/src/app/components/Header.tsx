d72035eecd59d07e8f74812626a0db2d7b5a3047
Artin <abdoullahiartin@gmail.com>
2026-02-07 13:08:47 +0000

    Update header

--- rugby_discover_cymru/src/app/components/Header.tsx
+++ rugby_discover_cymru/src/app/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
             <div className="left-section">
                 <div className="flex items-center gap-4">
                     <img src='/wru_assets/WRU_Primary_Logo.png' alt="Welsh Rugby Union Logo" className="header-logo" />
-                    <h1 className="text-4xl font-bold text-white header-title">Welcome to the WRU Discovery Tool</h1>
+                    <h1 className="text-4xl font-bold text-white header-title">DISCOVER WELSH RUGBY</h1>
                 </div>
             </div>
         </header>
